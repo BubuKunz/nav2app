ae2f11659df709ea2d5ddaed1960534b829a8402
yanzubritskiy <yan.zubritskiy@itomy.ch>
2020-05-14 14:14:50 +0300

    changed primary color

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nav2Up',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,8 +19,9 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        primaryColor: Color(0xff00838F)
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'where Near'),
     );
   }
 }
@@ -151,7 +152,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        leading: _isSearching ? const BackButton() : Container(),
+        leading: _isSearching ? const BackButton() : null,
         title: _isSearching ? _buildSearchField() : _buildTitle(),
         actions: _buildActions(),
       ),
