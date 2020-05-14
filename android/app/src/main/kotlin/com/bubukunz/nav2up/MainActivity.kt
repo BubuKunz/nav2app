fd0698975229ef1cf449c7bfc39dc8ff8f0fef21
yanzubritskiy <yan.zubritskiy@itomy.ch>
2020-05-14 13:03:07 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/bubukunz/nav2up/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bubukunz.nav2up
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
